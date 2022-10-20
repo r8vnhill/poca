3deb2cf41a02fd39b3630837bf5a80bf2e0a2cc5
R8V <ignacio.slater@ug.uchile.cl>
2022-10-20 10:49:45 -0300

    Property based

--- src/main/kotlin/cl/ravenhill/poca/Card.kt
+++ src/main/kotlin/cl/ravenhill/poca/Card.kt
@@ -1,12 +1,20 @@
 package cl.ravenhill.poca
 
 import java.util.*
+import kotlin.math.max
+import kotlin.math.min
 
 class Card(
   val name: String,
   val type: String,
-  val maxHp: Int
+  val maxHp: Int,
+  val attack: Int
 ) {
+  var hp: Int = maxHp
+    private set(newHp) {
+      field = max(0, min(newHp, maxHp))
+    }
+
   override fun equals(other: Any?) = when {
     this === other -> true
     other !is Card -> false
@@ -21,4 +29,12 @@ class Card(
 
   override fun toString() =
     "Card(name: '$name', type: '$type', maxHp: $maxHp)"
+
+  fun attack(opponent: Card) {
+    if (!this.isKo()) {
+      opponent.hp -= attack
+    }
+  }
+
+  fun isKo() = hp == 0
 }
--- src/test/kotlin/cl/ravenhill/poca/CardSpec.kt
+++ src/test/kotlin/cl/ravenhill/poca/CardSpec.kt
@@ -1,83 +1,77 @@
 package cl.ravenhill.poca
 
 import io.kotest.core.spec.style.WordSpec
-import io.kotest.datatest.withData
 import io.kotest.matchers.should
 import io.kotest.matchers.shouldBe
-import io.kotest.matchers.shouldNot
-import io.kotest.matchers.shouldNotBe
 import io.kotest.matchers.types.haveSameHashCodeAs
-
-private const val CHARIZARD_NAME = "Charizard"
-private const val CHARIZARD_TYPE = "Fire"
-private const val CHARIZARD_HP = 120
-private const val MEW_NAME = "Mew"
-private const val MEW_TYPE = "Psychic"
-private const val MEW_HP = 60
-private const val PIKACHU_NAME = "Pikachu"
-private const val PIKACHU_TYPE = "Electric"
-private const val PIKACHU_HP = 60
+import io.kotest.property.Arb
+import io.kotest.property.arbitrary.arbitrary
+import io.kotest.property.arbitrary.int
+import io.kotest.property.arbitrary.string
+import io.kotest.property.checkAll
 
 data class CardData(
     val name: String,
     val type: String,
-    val maxHp: Int
+    val maxHp: Int,
+    val attack: Int
 )
 
+fun cardArb(minHp: Int = 1, maxHp: Int = 100, minAtk: Int = 1, maxAtk: Int = 100) =
+    arbitrary {
+        val name = Arb.string().bind()
+        val type = Arb.string().bind()
+        val hp = Arb.int(minHp..maxHp).bind()
+        val attack = Arb.int(minAtk..maxAtk).bind()
+        CardData(name, type, hp, attack)
+    }
+
+private fun cardPair(card1: CardData, card2: CardData) =
+    Card(card1.name, card1.type, card1.maxHp, card1.attack) to
+            Card(card2.name, card2.type, card2.maxHp, card2.attack)
+
 class CardSpec : WordSpec({
-    lateinit var charizard: Card
-    lateinit var mew: Card
+    "Two cards with the same parameters" should {
+        "be equal" {
+            checkAll<String, String, Int, Int> { name, type, maxHp, attack ->
+                val card1 = Card(name, type, maxHp, attack)
+                val card2 = Card(name, type, maxHp, attack)
+                card1 shouldBe card2
+            }
+        }
 
-    beforeEach {
-        charizard = Card(
-            CHARIZARD_NAME,
-            CHARIZARD_TYPE,
-            CHARIZARD_HP
-        )
-        mew = Card(MEW_NAME, MEW_TYPE, MEW_HP)
-    }
-
-    "Two cards with the same parameters should be equal" When {
-        withData(
-            CardData(CHARIZARD_NAME, CHARIZARD_TYPE, CHARIZARD_HP),
-            CardData(MEW_NAME, MEW_TYPE, MEW_HP),
-            CardData(PIKACHU_NAME, PIKACHU_TYPE, PIKACHU_HP)
-        ) { (name, type, hp) ->
-            Card(name, type, hp) shouldBe Card(name, type, hp)
+        "have the same hash code" {
+            checkAll<String, String, Int, Int> { name, type, maxHp, attack ->
+                val card1 = Card(name, type, maxHp, attack)
+                val card2 = Card(name, type, maxHp, attack)
+                card1 should haveSameHashCodeAs(card2)
+            }
         }
     }
 
-    "Two cards with the same parameters should have the same hash code" When {
-        withData(
-            CardData(CHARIZARD_NAME, CHARIZARD_TYPE, CHARIZARD_HP),
-            CardData(MEW_NAME, MEW_TYPE, MEW_HP),
-            CardData(PIKACHU_NAME, PIKACHU_TYPE, PIKACHU_HP)
-        ) { (name, type, hp) ->
-            Card(name, type, hp) should haveSameHashCodeAs(Card(name, type, hp))
+    "An attack" should {
+        "deal damage to the target" {
+            checkAll(
+                cardArb(minHp = 50),
+                cardArb(maxAtk = 50)
+            ) { card1, card2 ->
+                val (c1, c2) = cardPair(card1, card2)
+                c2.attack(c1)
+                c1.hp shouldBe c1.maxHp - c2.attack
+
+            }
+        }
+        "be able to ko the target" {
+            checkAll(
+                cardArb(maxHp = 50),
+                cardArb(minAtk = 50)
+            ) { card1, card2 ->
+                val (c1, c2) = cardPair(card1, card2)
+                c2.attack(c1)
+                c1.isKo() shouldBe true
+
+            }
         }
     }
 
-    "Two cards with different parameters should not be equal" When {
-        withData(
-            CardData(CHARIZARD_NAME, CHARIZARD_TYPE, CHARIZARD_HP),
-            CardData(MEW_NAME, CHARIZARD_TYPE, CHARIZARD_HP),
-            CardData(CHARIZARD_NAME, PIKACHU_TYPE, CHARIZARD_HP)
-        ) { (name, type, hp) ->
-            Card(name, type, hp) shouldNotBe Card(name, type, hp + 1)
-            Card(name, type, hp) shouldNotBe Card(name, "Other type", hp)
-            Card(name, type, hp) shouldNotBe Card("Other name", type, hp)
-        }
-    }
-
-    "Two cards with different parameters should not have the same hash codes" When {
-        withData(
-            CardData(CHARIZARD_NAME, CHARIZARD_TYPE, CHARIZARD_HP),
-            CardData(MEW_NAME, CHARIZARD_TYPE, CHARIZARD_HP),
-            CardData(CHARIZARD_NAME, PIKACHU_TYPE, CHARIZARD_HP)
-        ) { (name, type, hp) ->
-            Card(name, type, hp) shouldNot haveSameHashCodeAs(Card(name, type, hp + 1))
-            Card(name, type, hp) shouldNot haveSameHashCodeAs(Card(name, "Other type", hp))
-            Card(name, type, hp) shouldNot haveSameHashCodeAs(Card("Other name", type, hp))
-        }
-    }
 })
