109b0664cbf09449cee4aacb3d319d1fc2bc4075
R8V <ignacio.slater@ug.uchile.cl>
2022-10-25 12:24:32 -0300

    REFACTOR Code style corrections

--- src/test/kotlin/cl/ravenhill/poca/CardSpec.kt
+++ src/test/kotlin/cl/ravenhill/poca/CardSpec.kt
@@ -14,52 +14,47 @@ private const val MEW_NAME = "Mew"
 private const val MEW_TYPE = "Psychic"
 private const val MEW_HP = 60
 
+/**
+ * Tests for [Card].
+ */
 class CardSpec : WordSpec({
-  lateinit var charizard: Card
-  lateinit var mew: Card
+    lateinit var charizard: Card
+    lateinit var mew: Card
 
-  beforeEach {
-    charizard = Card(CHARIZARD_NAME, CHARIZARD_TYPE, CHARIZARD_HP)
-    mew = Card(MEW_NAME, MEW_TYPE, MEW_HP)
-  }
-
-  "Two cards with the same parameters" should {
-    "Be equal" {
-      val charizard2 =
-        Card(CHARIZARD_NAME, CHARIZARD_TYPE, CHARIZARD_HP)
-      charizard shouldBe charizard2
-      val mew2 = Card(MEW_NAME, MEW_TYPE, MEW_HP)
-      mew shouldBe mew2
+    beforeEach {
+        charizard = Card(CHARIZARD_NAME, CHARIZARD_TYPE, CHARIZARD_HP)
+        mew = Card(MEW_NAME, MEW_TYPE, MEW_HP)
     }
 
-    "Have the same hash code" {
-      val charizard2 =
-        Card(CHARIZARD_NAME, CHARIZARD_TYPE, CHARIZARD_HP)
-      charizard should haveSameHashCodeAs(
-        charizard2
-      )
-      val mew2 = Card(MEW_NAME, MEW_TYPE, MEW_HP)
-      mew should haveSameHashCodeAs(mew2)
-    }
-  }
+    "Two cards with the same parameters" should {
+        "Be equal" {
+            val charizard2 = Card(CHARIZARD_NAME, CHARIZARD_TYPE, CHARIZARD_HP)
+            charizard shouldBe charizard2
+            val mew2 = Card(MEW_NAME, MEW_TYPE, MEW_HP)
+            mew shouldBe mew2
+        }
 
-  "Two Pokémon with different parameters" should {
-    "Not be equal" {
-      val charizard2 =
-        Card(CHARIZARD_NAME, CHARIZARD_TYPE, 100)
-      charizard shouldNotBe charizard2
-      val mew2 = Card(MEW_NAME, MEW_TYPE, 100)
-      mew shouldNotBe mew2
+        "Have the same hash code" {
+            val charizard2 = Card(CHARIZARD_NAME, CHARIZARD_TYPE, CHARIZARD_HP)
+            charizard should haveSameHashCodeAs(charizard2)
+            val mew2 = Card(MEW_NAME, MEW_TYPE, MEW_HP)
+            mew should haveSameHashCodeAs(mew2)
+        }
     }
 
-    "Not have the same hash code" {
-      val charizard2 =
-        Card(CHARIZARD_NAME, CHARIZARD_TYPE, 100)
-      charizard shouldNot haveSameHashCodeAs(
-        charizard2
-      )
-      val mew2 = Card(MEW_NAME, MEW_TYPE, 100)
-      mew shouldNot haveSameHashCodeAs(mew2)
+    "Two Pokémon with different parameters" should {
+        "Not be equal" {
+            val charizard2 = Card(CHARIZARD_NAME, CHARIZARD_TYPE, 100)
+            charizard shouldNotBe charizard2
+            val mew2 = Card(MEW_NAME, MEW_TYPE, 100)
+            mew shouldNotBe mew2
+        }
+
+        "Not have the same hash code" {
+            val charizard2 = Card(CHARIZARD_NAME, CHARIZARD_TYPE, 100)
+            charizard shouldNot haveSameHashCodeAs(charizard2)
+            val mew2 = Card(MEW_NAME, MEW_TYPE, 100)
+            mew shouldNot haveSameHashCodeAs(mew2)
+        }
     }
-  }
 })
